b1ebd3b5fb45fbb0df3ee7bb16abe180ba996b9f
GabrieleRusso11 <93495918+GabrieleRusso11@users.noreply.github.com>
2022-06-18 23:39:36 +0200

    Add files via upload

--- src/UI.cpp
+++ src/UI.cpp
@@ -14,23 +14,26 @@ int main(int argc, char** argv){
     ros::ServiceClient client_goal = n.serviceClient<rt1_third_assignment::Goal>("/goal_position");
 
     ros::ServiceClient client_interface = n.serviceClient<rt1_third_assignment::Interface>("/commands");
-    //ros::ServiceClient client_interface = n.serviceClient<rt1_third_assignment::Interface>("/commands");
 
     rt1_third_assignment::Goal pos;
 
     rt1_third_assignment::Interface com;
 
     while(ros::ok()){
+
         int command = 0;
-        cout<<"choose the modality : "<<endl;
+        cout<<"************************************************"<<endl;
+        cout<<endl<<"choose the modality : "<<endl;
         cout<<endl<<" * auotonomus drive -> press 1 "<<endl;
         cout<<endl<<" * manual drive -> press 2 "<<endl;
         cout<<endl<<" * driving assistance -> press 3"<<endl;
-        cout<<endl<<" * cancel the goal -> press 4 "<<endl;
+        cout<<endl<<" * cancel the goal -> press 4"<<endl;
+        cout<<endl<<" * Exit -> press 0 "<<endl;
+        cout<<endl<<"************************************************"<<endl;
         cin>>command;
 
-        if(command == 1){
-            
+        if(command == 1){  // the user has chosen the auto drive mode
+            int canc = 0;
             com.request.command = 'a';
             client_interface.waitForExistence();
             client_interface.call(com);
@@ -45,11 +48,8 @@ int main(int argc, char** argv){
 
             cout<<"the goal position is sent to the controller"<<endl;
 
-            if(pos.response.feedback == true){
-                cout<<"OKAY : The Goal is Achieved."<<endl;
-            }
         }
-        else if(command == 2 ){
+        else if(command == 2 ){ // the user has chosen the manual drive mode
             
             cout<<"Now You are in the manual drive mode, You can use teleop to conntrol the robot."<<endl;
             com.request.command = 'm';
@@ -58,7 +58,7 @@ int main(int argc, char** argv){
              
 
         }
-        else if(command == 3){
+        else if(command == 3){ // the user has chosen the assisted drive mode
 
             cout<<"Now You are in the assisted drive mode"<<endl;
             com.request.command = 'd';
@@ -67,14 +67,23 @@ int main(int argc, char** argv){
              
 
         }
-        else if(command == 4){
+        else if(command == 4){ // the user has chosen to cancel its desired goal given in the auto drive mode
+
             com.request.command = 'c';
             client_interface.waitForExistence();
             client_interface.call(com);
+
         }
-        else if(command == 0){
+        else if(command == 0){ // the user has chosen to exit from the user interface
 
             exit(0);
+
+        }
+        else{
+
+            cout<<endl<<"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"<<endl;
+            cout<<"You have pressed the wrong button, please be careful."<<endl;
+            cout<<"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"<<endl;
 
         }
 
